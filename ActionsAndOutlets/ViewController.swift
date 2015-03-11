9903d7befd4446186f11ed3ae4c74417ebc5974b
Chrisna Aing <chrisna@chrisna.org>
2015-03-11 11:54:37 -0700

    Make stuff private.

--- ActionsAndOutlets/ViewController.swift
+++ ActionsAndOutlets/ViewController.swift
@@ -12,12 +12,14 @@ class ViewController: UIViewController {
 
     // MARK: - Outlet Collection
 
-    // Order is not guaranteed in outlet collections.
-    @IBOutlet var switches: [UISwitch]!
+    // Order is not guaranteed in outlet collections. And, things should be private whenever it's
+    // possible to make them private!
+
+    @IBOutlet private var switches: [UISwitch]!
 
     // MARK: - First
 
-    @IBAction func myAction(sender: AnyObject) {
+    @IBAction private func myAction(sender: AnyObject) {
         // This is the default configuration of an action. Note that sender is an object of type
         // AnyObject, which, if you want to use the sender, is probably not what you want. This is
         // because AnyObject allows anything to happen. The code below, if uncommented, will crash
@@ -25,7 +27,7 @@ class ViewController: UIViewController {
         // sender.setOn(true, animated: true)
     }
 
-    @IBAction func myActionImproved(sender: UIButton) {
+    @IBAction private func myActionImproved(sender: UIButton) {
         // Much better! Now the compiler will warn us if we try what we did above. This is also an
         // example of why having the sender is useful: sometimes, we need to modify it in some way.
         sender.setTitle("First!", forState: .Normal)
@@ -40,11 +42,11 @@ class ViewController: UIViewController {
     // Also, to better align with best practices, we do not pass an argument into the action because
     // we don't need one.
 
-    @IBAction func touchOutside() {
+    @IBAction private func touchOutside() {
         println("Touch up outside!")
     }
 
-    @IBAction func touch() {
+    @IBAction private func touch() {
         println("Touch up inside!")
     }
 
@@ -53,7 +55,7 @@ class ViewController: UIViewController {
     // Passing in the event can be helpful if you want very specific information about a user's
     // interaction with the screen.
 
-    @IBAction func thirdAction(sender: UIButton, forEvent event: UIEvent) {
+    @IBAction private func thirdAction(sender: UIButton, forEvent event: UIEvent) {
         // This doesn't work in Swift 1.2 / Xcode 6.3 because allTouches() returns a Swift Set (a
         // new collection type in Swift 1.2) rather than an NSSet (an artifact of Objective-C and
         // Foundation, like NSArray and NSDictionary).
@@ -69,13 +71,13 @@ class ViewController: UIViewController {
     // Instead of doing this with individual outlets, we can create an outlet collection and iterate
     // through the list of UISwitches. This is way cleaner and less error-prone as well.
 
-    @IBAction func toggleOn() {
+    @IBAction private func toggleOn() {
         for s in switches {
             s.setOn(true, animated: true)
         }
     }
 
-    @IBAction func toggleOff() {
+    @IBAction private func toggleOff() {
         for s in switches {
             s.setOn(false, animated: true)
         }
